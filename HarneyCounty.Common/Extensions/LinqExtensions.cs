24232074712e2d11fa90a0a84aa701dbca1ddfa7
Ahmed Sayed <Ahmed.Sayed@itworx.com>
2017-10-27 21:28:20 +0300

    -FIlter by feature

--- HarneyCounty.Common/Extensions/LinqExtensions.cs
+++ HarneyCounty.Common/Extensions/LinqExtensions.cs
@@ -37,5 +37,25 @@ namespace HarneyCounty.Common.Extensions
         {
             return original ?? new T[0];
         }
+
+        /// <summary>
+        /// Return distinct collection using key selector comparer
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> knownKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (knownKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
--- HarneyCounty.Web/Controllers/HomeController.cs
+++ HarneyCounty.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using HarneyCounty.Application.Core.Contracts;
 using HarneyCounty.Application.Core.Contracts.Paging;
 using HarneyCounty.Application.Core.Interfaces;
+using HarneyCounty.Common.Extensions;
 using HarneyCounty.Domain.Core.Models;
 using HarneyCounty.Web.Extensions;
 using HarneyCounty.Web.ViewModel;
@@ -33,17 +34,9 @@ namespace HarneyCounty.Web.Controllers
             {
                 ViewBag.DisplayResults = false;
             }
-
             ViewBag.FilterViewModel = filter;
-            var viewmodel = entities.ToMappedPagedList<AccountMasterFullDetail, AccountMasterViewModel>(pagingInfo);
-            if (viewmodel != null && viewmodel.Any() && string.IsNullOrWhiteSpace(filter.AccountNumber) && string.IsNullOrWhiteSpace(filter.OwnerName))
-            {
-                foreach (var item in viewmodel)
-                {
-                    item.OwnerName = item.CareOfFlag == "%" ? item.CareOfName : item.OwnerName;
-                }
-            }
-            else
+            //Search by owner name
+            if (filter.SearchBy == SearchByType.OwnerName)
             {
                 entities = entities.Select(x => new AccountMasterFullDetail
                 {
@@ -62,7 +55,129 @@ namespace HarneyCounty.Web.Controllers
                     PropClassCode = x.PropClassCode,
                     CodeAreaCode = x.CodeAreaCode,
                     RollType = x.RollType
-                }).Distinct(new CompareTwoItems()).ToList();
+                }).DistinctBy(c => c.AsmtYear).DistinctBy(c => c.OwnerName).DistinctBy(c => c.Twnshp).
+                   DistinctBy(c => c.TwnshpDir).DistinctBy(c => c.Range).DistinctBy(c => c.RangDir).DistinctBy(c => c.Sctn).DistinctBy(c => c.QtrSctn).
+                   DistinctBy(c => c.Prcl).DistinctBy(c => c.SpecIntAlph).DistinctBy(c => c.SpecIntNmbr).DistinctBy(c => c.PropClassCode).
+                   DistinctBy(c => c.CodeAreaCode);
+            }
+            else if(filter.SearchBy == SearchByType.Situs)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    SitusStrtNmbr = c.SitusStrtNmbr,
+                    SitusStrtNmbrSufx = c.SitusStrtNmbrSufx,
+                    SitusStrtDir = c.SitusStrtDir,
+                    SitusStrtName = c.SitusStrtName,
+                    SitusStrtNameSufx = c.SitusStrtNameSufx,
+                    SitusZipCode = c.SitusZipCode,
+                    OwnerName = c.CareOfFlag == "%" ? c.CareOfFlag : c.OwnerName
+                });
+            }
+            else if(filter.SearchBy == SearchByType.SubBlockLot)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    AcctNmbr = c.AcctNmbr,
+                    SbdvnCode = c.SbdvnCode,
+                    BlckNmbr = c.BlckNmbr,
+                    LotNmbr = c.LotNmbr,
+                    PropClassCode = c.PropClassCode,
+                    CodeAreaCode = c.CodeAreaCode,
+                    OwnerName = c.CareOfFlag == "%" ? c.CareOfFlag : c.OwnerName
+                });
+            }
+            else if(filter.SearchBy == SearchByType.Map)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    AcctNmbr = c.AcctNmbr,
+                    Twnshp = c.Twnshp,
+                    TwnshpDir = c.TwnshpDir,
+                    Range = c.Range,
+                    RangDir = c.RangDir,
+                    Sctn = c.Sctn,
+                    QtrSctn = c.QtrSctn,
+                    Prcl = c.Prcl,
+                    SpecIntAlph = c.SpecIntAlph,
+                    SpecIntNmbr = c.SpecIntNmbr,
+                    PropClassCode = c.PropClassCode,
+                    CodeAreaCode = c.CodeAreaCode,
+                    RollType = c.RollType,
+                    OwnerName = c.CareOfFlag == "%" ? c.CareOfFlag : c.OwnerName
+                });
+            }
+            else if(filter.SearchBy == SearchByType.XNumberOrMobileHomeId)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    AcctNmbr = c.AcctNmbr,
+                    MobileHomeId = c.MobileHomeId,
+                    XNmbr = c.XNmbr,
+                    Twnshp = c.Twnshp,
+                    TwnshpDir = c.TwnshpDir,
+                    Range = c.Range,
+                    RangDir = c.RangDir,
+                    Sctn = c.Sctn,
+                    QtrSctn = c.QtrSctn,
+                    Prcl = c.Prcl,
+                    SpecIntAlph = c.SpecIntAlph,
+                    SpecIntNmbr = c.SpecIntNmbr,
+                    MobHomeRollType = c.MobileHomeRollType,
+                    OwnerName = c.CareOfFlag == "%" ? c.CareOfFlag : c.OwnerName
+                });
+            }
+            else if(filter.SearchBy == SearchByType.MobileHomeManufacturerOrMobileSerial)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    MobHomeMnfr = c.MobHomeMnfr,
+                    MHSerial = c.MhSerial,
+                    XNmbr = c.XNmbr,
+                    YearBuilt = c.YearBuilt,
+                    MobHomeWdth = c.MobHomeWdth,
+                    MobHomeLngth = c.MobHomeLngth,
+                    AcctNmbr01431 = c.AcctNmbr01431,
+                    MobHomeRollType = c.MobileHomeRollType,
+                    MobHomeSizeFlag = c.MobHomeSizeFlag,
+                });
+            }
+            else if(filter.SearchBy == SearchByType.PropertyClass)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    AcctNmbr = c.AcctNmbr,
+                    PropClassCode = c.PropClassCode,
+                    MntnAreaCode = c.MntnAreaCode,
+                    ValAreaCode = c.ValAreaCode,
+                    CodeAreCode = c.CodeAreaCode,
+                    RollType = c.RollType,
+                    OwnerName = c.CareOfFlag == "%" ? c.CareOfFlag : c.OwnerName
+                });
+            }
+            else if(filter.SearchBy == SearchByType.CodeArea)
+            {
+                entities = entities.ToList().DistinctBy(c => new
+                {
+                    AcctNmbr = c.AcctNmbr,
+                    CodeAreCode = c.CodeAreaCode,
+                    PropClassCode = c.PropClassCode,
+                    MntnAreaCode = c.MntnAreaCode,
+                    ValAreaCode = c.ValAreaCode,
+                    RollType = c.RollType,
+                    OwnerName = c.CareOfFlag == "%" ? c.CareOfFlag : c.OwnerName
+                });
+            }
+            var viewmodel = entities.ToMappedPagedList<AccountMasterFullDetail, AccountMasterViewModel>(pagingInfo);
+            if (viewmodel != null && viewmodel.Any() && string.IsNullOrWhiteSpace(filter.AccountNumber) && string.IsNullOrWhiteSpace(filter.OwnerName))
+            {
+
+                foreach (var item in viewmodel)
+                {
+                    item.OwnerName = item.CareOfFlag == "%" ? item.CareOfName : item.OwnerName;
+                }
+            }
+            else
+            {
                 viewmodel = entities.ToMappedPagedList<AccountMasterFullDetail, AccountMasterViewModel>(pagingInfo);
             }
 
@@ -72,22 +187,4 @@ namespace HarneyCounty.Web.Controllers
         }
     }
 
-    public class CompareTwoItems : IEqualityComparer<AccountMasterFullDetail>
-    {
-        public bool Equals(AccountMasterFullDetail x, AccountMasterFullDetail y)
-        {
-            if (x.AsmtYear == y.AsmtYear && x.OwnerName == y.OwnerName && x.Twnshp == y.Twnshp
-            && x.TwnshpDir == y.TwnshpDir && x.Range == y.Range && x.RangDir == y.RangDir && x.Sctn == y.Sctn && x.QtrSctn == y.QtrSctn && x.Prcl == y.Prcl
-            && x.SpecIntAlph == y.SpecIntAlph && x.SpecIntNmbr == y.SpecIntNmbr && x.PropClassCode == y.PropClassCode && x.CodeAreaCode == y.CodeAreaCode)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public int GetHashCode(AccountMasterFullDetail obj)
-        {
-            return obj.AsmtYear.GetHashCode();
-        }
-    }
 }
