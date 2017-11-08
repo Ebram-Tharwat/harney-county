c58a6e9c00f9f688297e1bbc1a912cebbfaf668e
Ebram <ebram_tharwat@outlook.com>
2017-11-09 00:48:36 +0200

    - add zipcode file repository.

---
+++ HarneyCounty.Infrastructure.Core/Repositories/ZipCodeFileRepository.cs
@@ -0,0 +1,23 @@
+﻿using HarneyCounty.Domain.Core.Models;
+using HarneyCounty.Infrastructure.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarneyCounty.Infrastructure.Core.DAL;
+
+namespace HarneyCounty.Infrastructure.Core.Repositories
+{
+    public class ZipCodeFileRepository : EFRepository<ZipCodeFile>, IZipCodeFileRepository
+    {
+        public ZipCodeFileRepository(HarneyCountyDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public List<ZipCodeFile> GetZipCodes(List<string> zipCodes)
+        {
+            return _dbContext.ZipCodeFiles.Where(z => zipCodes.Contains(z.ZipCode.Trim())).ToList();
+        }
+    }
+}
