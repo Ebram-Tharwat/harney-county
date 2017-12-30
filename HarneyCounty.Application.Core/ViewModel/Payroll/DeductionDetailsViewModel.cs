955364989f2729ff4d88bf60abedbea2183c119f
Ebram <ebram_tharwat@outlook.com>
2017-12-30 22:52:06 +0200

    - fix deductions totals

--- HarneyCounty.Application.Core/ViewModel/Payroll/DeductionDetailsViewModel.cs
+++ HarneyCounty.Application.Core/ViewModel/Payroll/DeductionDetailsViewModel.cs
@@ -40,11 +40,5 @@ namespace HarneyCounty.Application.Core.ViewModel.Payroll
 
         [Display(Name = "Description")]
         public string DeductionDesc { get; set; }
-
-        [Display(Name = "Total")]
-        public decimal? TotalAmount
-        {
-            get { return this.EmployeeAmt + this.EmployerAmt + this.EmployeeAmtPickedUp; }
-        }
     }
 }
